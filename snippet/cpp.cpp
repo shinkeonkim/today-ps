14e909fb0d95af3ff843c86b938b654a2c5592b0
shinkeonkim <singun11@naver.com>
2020-05-03 21:32:15 +0900

    add cpp snippet

---
+++ snippet/cpp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef unsigned int uint;
+typedef vector <ull> ullv1;
+typedef vector <vector <ull>> ullv2;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+}
