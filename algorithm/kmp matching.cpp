9a1cff5c39e1984eb220bd57de145bac626081da
shinkeonkim <singun11@naver.com>
2020-07-12 23:39:17 +0900

    algorithm: kmp

---
+++ algorithm/kmp matching.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#define MX 123456
+
+using namespace std;
+
+string content;
+string obj;
+
+int fail[MX];
+vector <int> kmp (string s, string o) {
+    fill(fail,fail+MX,0);
+    vector<int> result;
+    int N = s.length();
+    int M = o.length();
+    for(int i=1, j=0; i<M; i++){
+        while(j > 0 && o[i] != o[j]) j = fail[j-1];
+        if(o[i] == o[j]) fail[i] = ++j;
+    }
+    for(int i = 0, j = 0; i < N; i++) {
+        while(j > 0 && s[i] != o[j]) {
+            j = fail[j-1];
+        }
+        if(s[i] == o[j]) {
+            if(j == M-1) {
+                // matching OK;
+                result.push_back(i - M + 2);
+                j = fail[j];
+            }
+            else {
+                j++;
+            }
+        }
+    }
+    return result;
+}
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> content >> obj;
+    vector <int> ret = kmp(content, obj);
+    for(auto i : ret) {
+        cout << i << " ";
+    }
+}
